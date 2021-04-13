5c9bd1cc2ad270dbbe7f993cfd8f012a97928652
Rob Waller <rdwaller1984@gmail.com>
2021-04-13 10:35:08 +0100

    Created first functional test with pseudo code to test phpunit configuration.

---
+++ tests/Functional/APITest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Functional;
+
+use PHPUnit\Framework\TestCase;
+
+class APITest extends TestCase
+{
+    public function testAPIToken(): void
+    {
+        $this->assertTrue(true);
+    }
+}
