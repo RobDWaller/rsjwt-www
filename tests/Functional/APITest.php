3b5ca3f306ab072ad963038ea57da2bb30fa6d12
Rob Waller <rdwaller1984@gmail.com>
2021-04-13 10:51:02 +0100

    Updated functional test to check api/token endpoint.

--- tests/Functional/APITest.php
+++ tests/Functional/APITest.php
@@ -8,6 +8,17 @@ class APITest extends TestCase
 {
     public function testAPIToken(): void
     {
-        $this->assertTrue(true);
+        $client = new \GuzzleHttp\Client(['port' => '8080']);
+        $response = $client->request('GET', 'http://localhost/api/token');
+
+        $this->assertSame(200, $response->getStatusCode());
+        $this->assertStringContainsString('application/json', $response->getHeaderLine('content-type'));
+
+        $body = json_decode($response->getBody());
+
+        $this->assertMatchesRegularExpression(
+            '/^[a-zA-Z0-9\-\_\=]+\.[a-zA-Z0-9\-\_\=]+\.[a-zA-Z0-9\-\_\=]+$/',
+            $body->token
+        );
     }
 }
