e2f3385f738d2a73655829e80a882caee5e430d7
Rob Waller <rdwaller1984@gmail.com>
2021-05-10 13:24:26 +0100

    Updated functional tests to work based environment variables.

--- tests/Functional/APITest.php
+++ tests/Functional/APITest.php
@@ -6,10 +6,22 @@ use PHPUnit\Framework\TestCase;
 
 class APITest extends TestCase
 {
+    private string $baseUrl;
+
+    private string $port;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->baseUrl = $_SERVER['TEST_URL'] ?? 'http://localhost';
+        $this->port = $_SERVER['TEST_PORT'] ?? '80';
+    }
+
     public function testAPIToken(): void
     {
-        $client = new \GuzzleHttp\Client(['port' => '8080']);
-        $response = $client->request('GET', 'http://localhost/api/token');
+        $client = new \GuzzleHttp\Client(['port' => $this->port]);
+        $response = $client->request('GET', $this->baseUrl . '/api/token');
 
         $this->assertSame(200, $response->getStatusCode());
         $this->assertStringContainsString('application/json', $response->getHeaderLine('content-type'));
