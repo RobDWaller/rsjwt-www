02e40bf0a5bdc3e248c6c6768ad59f203b690129
Rob Waller <rdwaller1984@gmail.com>
2021-07-21 13:45:41 +0100

    Updated JWTFactory to use ReallySimpleJWT Build class as this is much more flexible.

--- index.php
+++ index.php
@@ -4,13 +4,18 @@ use DI\Container;
 use RSJWT\App;
 use Slim\Factory\AppFactory;
 use RSJWT\JWT\JWTFactory;
+use ReallySimpleJWT\Build;
+use ReallySimpleJWT\Secret;
+use ReallySimpleJWT\Helper\Validator;
+use ReallySimpleJWT\Encoders\EncodeHS256;
 
 require __DIR__ . '/vendor/autoload.php';
 
 $container = new Container();
 
 $container->set('jwtFactory', function () {
-    return new JWTFactory();
+    $build = new Build('JWT', new Validator(), new Secret(), new EncodeHS256());
+    return new JWTFactory($build);
 });
 
 AppFactory::setContainer($container);
--- src/JWT/JWTFactory.php
+++ src/JWT/JWTFactory.php
@@ -5,16 +5,28 @@ declare(strict_types=1);
 namespace RSJWT\JWT;
 
 use RSJWT\JWT\JWT;
-use ReallySimpleJWT\Token;
+use ReallySimpleJWT\Build;
 
 class JWTFactory
 {
+    private Build $build;
+
+    public function __construct(Build $build)
+    {
+        $this->build = $build;
+    }
+
     /**
      * @param mixed $id
      */
     public function create($id, string $secret, int $expiration, string $issuer): JWT
     {
-        $token = Token::create($id, $secret, $expiration, $issuer);
-        return new JWT($token, $secret);
+        $jwt = $this->build->setJwtId($id)
+            ->setExpiration($expiration)
+            ->setIssuer($issuer)
+            ->setSecret($secret)
+            ->build();
+            
+        return new JWT($jwt->getToken(), $secret);
     }
 }
