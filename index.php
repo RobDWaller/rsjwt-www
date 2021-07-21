bfe6a37ad32566920403ef7fa3506ac4b9621003
Rob Waller <rdwaller1984@gmail.com>
2021-07-21 12:01:49 +0100

    Updated JWT Factory class to be build and retrieved from the DI container.

--- index.php
+++ index.php
@@ -1,10 +1,19 @@
 <?php
 
+use DI\Container;
 use RSJWT\App;
 use Slim\Factory\AppFactory;
+use RSJWT\JWT\JWTFactory;
 
 require __DIR__ . '/vendor/autoload.php';
 
+$container = new Container();
+
+$container->set('jwtFactory', function () {
+    return new JWTFactory();
+});
+
+AppFactory::setContainer($container);
 $app = AppFactory::create();
 
 $rsjwt = new App($app);
